175a63ed46ea4aa2e85aba2993ddce221c2bcc52
Y Sussman <ypaulsussman@gmail.com>
2021-05-18 14:34:50 -0500

    upgrade deps

--- src/App/App.test.jsx
+++ src/App/App.test.jsx
@@ -4,6 +4,6 @@ import App from "./App.jsx";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/save to reload/i);
+  const linkElement = screen.getByText(/Welcome to MOkko/i);
   expect(linkElement).toBeInTheDocument();
 });
